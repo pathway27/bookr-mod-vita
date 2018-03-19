e50d7c6a71a2140b9ba58b243f57abcfc48b892d
pathway27 <pathway27@gmail.com>
2018-03-19 19:14:17 +1100

    fix(utils): remove vita2d texture gen. cast
    one warning is ok ;)

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,7 +13,7 @@ vita2d_texture* _vita2d_load_pixmap_generic(fz_pixmap *pixmap)
 
 
   for (int y = 0; y < height; ++y) {
-    unsigned int *tex_pointer = (unsigned int *)((unsigned int *)texture_data + y*tex_stride);
+    unsigned int *tex_pointer = (unsigned int *)(texture_data + y*tex_stride);
     unsigned char *pixels_row = &pixmap->samples[y * pixmap->stride];
 
     for (int x = 0; x < width; ++x) {
